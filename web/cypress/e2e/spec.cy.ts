c4aac1c8511cdc85b5ec6368a6d79d820d36fcfc
Stefan Homan <stefanhoman56@gmail.com>
2023-03-01 18:34:05 +0200

    Edit e2e test

--- web/cypress/e2e/spec.cy.ts
+++ web/cypress/e2e/spec.cy.ts
@@ -1,7 +1,32 @@
-describe('empty spec', () => {
+describe('Settle up', () => {
   it('passes', () => {
     cy.visit('http://localhost:4200');
-    cy.get('[data-cy="test"]').contains('Hello!!');
-    cy.get('[data-cy="tab1"]').contains(' Content 1 ');
+
+    const expenses = [
+      { "name": "Adriana", "amount": 5.75 },
+      { "name": "Adriana", "amount": 5.75 },
+      { "name": "Bao", "amount": 12 }
+    ];
+    for (const expense of expenses) {
+      cy.get('[data-cy="input-name"]').type(expense.name);
+      cy.get('[data-cy="input-amount"]').contains(expense.amount);
+      cy.get('[data-cy="add-button"]').click();
+    }
+
+    cy.get('[data-cy="expenses-table"] tbody tr').should('have.length', 4);
+
+    cy.get('[data-cy="settleup-button"]').click();
+
+    cy.get('[data-cy="payouts-response"]').contains(`{
+      "total": 23.5,
+      "equalShare": 11.75,
+      "payouts": [
+        {
+          "owes": "Adriana",
+          "owed": "Bao",
+          "amount": 0.25
+        }
+      ]
+    }`);
   })
 })
