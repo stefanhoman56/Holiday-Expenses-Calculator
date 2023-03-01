3f2c6bfca14748b723f6d436a0b0c75f349d7b2c
Stefan Homan <stefanhoman56@gmail.com>
2023-03-01 11:54:10 +0200

    Add payouts api and write test

--- api/__tests__/app.test.ts
+++ api/__tests__/app.test.ts
@@ -9,6 +9,77 @@ describe('GET /hello', () => {
     });
 });
 
+describe('POST /expenses', () => {
+    test('should return "Invalid parameters"', async () => {
+        const response = await request(app)
+            .post('/payouts')
+            .set('Accept', 'application/json');
+
+        expect(response.status).toBe(400);
+        expect(response.text).toBe("Invalid parameters");
+    });
+
+    test('Check with 2 people', async () => {
+        const params = {
+            "expenses": [
+                { "name": "Adriana", "amount": 5.75 },
+                { "name": "Adriana", "amount": 5.75 },
+                { "name": "Bao", "amount": 12 }
+            ]
+        };
+
+        const response = await request(app)
+            .post('/payouts')
+            .send(params)
+            .set('Accept', 'application/json');
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            "total": 23.5,
+            "equalShare": 11.75,
+            "payouts": [
+                {
+                    "owes": "Adriana",
+                    "owed": "Bao",
+                    "amount": 0.25
+                }
+            ]
+        });
+    });
+
+    test('Check with 3 people', async () => {
+        const params = {
+            "expenses": [
+                { "name": "Adriana", "amount": 5.75 },
+                { "name": "Laura", "amount": 4.75 },
+                { "name": "Bao", "amount": 12 }
+            ]
+        };
+
+        const response = await request(app)
+            .post('/payouts')
+            .send(params)
+            .set('Accept', 'application/json');
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            "total": 22.5,
+            "equalShare": 7.5,
+            "payouts": [
+                {
+                    "owes": "Adriana",
+                    "owed": "Bao",
+                    "amount": 1.75
+                }, {
+                    "owes": "Laura",
+                    "owed": "Bao",
+                    "amount": 2.75
+                }
+            ]
+        });
+    });
+});
+
 afterAll((done) => {
     server.close(done);
 });
--- api/src/app.ts
+++ api/src/app.ts
@@ -1,18 +1,32 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import { Expense, PayoutsCalculator } from './services/payout';
 
 const app = express();
 const port = 3000;
 
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(cors());
+app
+  .use(express.urlencoded({ extended: true }))
+  .use(cors())
+  .use(bodyParser.json());
 
 app.get('/hello', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.post('/payouts', (req: Request, res: Response) => {
+  const expenses: Expense[] = req.body.expenses;
+
+  // Validate request parameters
+  if (!expenses || !Array.isArray(expenses)) {
+    res.status(400).send('Invalid parameters');
+    return;
+  }
+
+  // Calculate the payouts
+  const payoutsCalculator = new PayoutsCalculator(expenses);
+  res.json(payoutsCalculator.calculate());
 });
 
 const server = app.listen(port, () => {
